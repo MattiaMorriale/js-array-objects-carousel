ea2d4381f9961700c24c4e974a24266048e3636f
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-16 21:59:03 +0100

    fine dei bonus, fine dell'esercizio

--- js/script.js
+++ js/script.js
@@ -164,3 +164,67 @@ function showSlide(number) {
 
 console.log(formaggi)
 
+const playButton = document.querySelector("#play-button");
+const pauseButton = document.querySelector("#pause-button");
+const reverseButton = document.querySelector("#reverse-button");
+
+
+
+
+let timer;
+
+playButton.addEventListener("click", () => {
+
+    playButton.style.display = "none";
+    pauseButton.style.display = "inline-block";
+    reverseButton.style.display = "none";
+
+
+    timer = setInterval(function() {
+    
+        if(slideNumber > images.length -1) {
+            slideNumber = 1;
+            showSlide(slideNumber);
+    
+        } else {
+    
+            slideNumber++;
+            showSlide(slideNumber);
+        }
+    
+    }, 3000);
+});
+
+
+pauseButton.addEventListener("click", () => {
+    playButton.style.display = "inline-block";
+    reverseButton.style.display = "inline-block"
+    pauseButton.style.display = "none";
+
+    clearInterval(timer);
+});
+
+
+reverseButton.addEventListener("click", () => {
+    reverseButton.style.display = "none"
+    playButton.style.display = "none";
+    pauseButton.style.display = "inline-block";
+
+
+
+    timer = setInterval(function() {
+    
+        if(slideNumber <= 1) {
+            slideNumber = images.length;
+            showSlide(slideNumber);
+    
+        } else {
+    
+            slideNumber--;
+            showSlide(slideNumber);
+        }
+    
+    }, 3000);
+
+}); 
+
